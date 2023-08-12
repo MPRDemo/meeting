868e8a9194ee5308c81cfd09c0f577f81b94e9c4
MPRDemo <142082279+MPRDemo@users.noreply.github.com>
2023-08-12 13:24:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,285 @@
+let content = []; // Initialize an empty array to hold the content
+const storedContent = localStorage.getItem('bookedSlots');
+if (storedContent) {
+  content = JSON.parse(storedContent);
+}
+
+const date = new Date();
+const timeSlots = [
+  { time: "9:30 am - 10:00 am", booked: false },
+  { time: "10:00 am - 10:30 am", booked: false },
+  { time: "10:30 am - 11:00 am", booked: false },
+  { time: "11:00 am - 11:30 am", booked: false },
+  { time: "11:30 am - 12:00 pm", booked: false },
+  { time: "12:00 pm - 12:30 pm", booked: false },
+  { time: "12:30 pm - 1:00 pm", booked: false },
+  { time: "1:00 pm - 1:30 pm", booked: false },
+  { time: "1:30 pm - 2:00 pm", booked: false },
+  { time: "2:00 pm - 2:30 pm", booked: false },
+  { time: "2:30 pm - 3:00 pm", booked: false },
+  { time: "3:00 pm - 3:30 pm", booked: false },
+  { time: "3:30 pm - 4:00 pm", booked: false },
+  { time: "4:00 pm - 4:30 pm", booked: false },
+  { time: "4:30 pm - 5:00 pm", booked: false },
+  { time: "5:00 pm - 5:30 pm", booked: false },
+  { time: "5:30 pm - 6:00 pm", booked: false },
+  { time: "6:00 pm - 6:30 pm", booked: false },
+  { time: "6:30 pm - 7:00 pm", booked: false },
+  { time: "7:00 pm - 7:30 pm", booked: false },
+  { time: "7:30 pm - 8:00 pm", booked: false },
+  { time: "8:00 pm - 8:30 pm", booked: false },
+  { time: "8:30 pm - 9:00 pm", booked: false },
+  { time: "9:00 pm - 9:30 pm", booked: false }
+];
+
+const teams = [
+  "Info",
+  "Incident",
+  "Food & Procurement",
+  "Security & Laundry",
+  "IT",
+  "Account SBHL",
+  "Account AE",
+  "Account RAH",
+  "MPR",
+  "Maintenance",
+  "Care Support",
+  "Training",
+  "Data Analyst",
+  "Developer",
+  "AEX & CEX",
+  "HR/GM"
+];
+
+const showDateContent = (date) => {
+  const dateContent = document.getElementById("dateContent");
+
+  // Convert the clicked date to a string in the "YYYY-MM-DD" format
+  const clickedDateString = date.toISOString().split('T')[0];
+
+  // Find the content entry that matches the clicked date
+  const selectedContent = content.find((item) => item.date === clickedDateString);
+
+  const slotsHtml = timeSlots.map((slot) => {
+    if (selectedContent && selectedContent.slots.includes(slot.time)) {
+      return `<div class="booked-slot">${slot.time} - Booked</div>`;
+    } else if (!slot.booked) {
+      return `<div class="time-slot" data-booked="${slot.booked}" onclick="bookSlotConfirmation('${slot.time}', '${clickedDateString}')">${slot.time}</div>`;
+    } else {
+      return `<div class="time-slot booked">${slot.time} - Booked</div>`;
+    }
+  }).join('');
+
+  dateContent.innerHTML = `<h2 class="date-heading">${date.toDateString()}</h2><div class="slots-container">${slotsHtml}</div>`;
+  dateContent.style.display = "block";
+};
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const daysDiv = document.querySelector(".days");
+  const monthDays = document.querySelector(".days");
+  const yearLabel = document.querySelector(".year-label");
+  const currentYear = date.getFullYear();
+  yearLabel.querySelector("p").textContent = currentYear;
+
+  let days = ""; 
+
+const bookSlotConfirmation = (slot, date) => {
+  const teamNameModal = document.getElementById('teamNameModal');
+  const teamNameSelect = document.getElementById('teamNameSelect');
+
+  // Clear selected team and show the team selection modal
+  teamNameSelect.value = '';
+  teamNameModal.style.display = 'block';
+
+  // Handle submit and cancel buttons
+  const teamNameSubmit = document.getElementById('teamNameSubmit');
+  const teamNameCancel = document.getElementById('teamNameCancel');
+
+  teamNameSubmit.onclick = () => {
+    const selectedTeam = teamNameSelect.value;
+    if (selectedTeam) {
+      // Set confirmation message
+      const confirmationMessage = `Do you want to book the slot: ${slot} for Team: ${selectedTeam}?`;
+      document.getElementById('confirmationMessage').textContent = confirmationMessage;
+
+      // Hide the team selection modal
+      teamNameModal.style.display = 'none';
+
+      // Show the confirmation modal
+      const confirmationModal = document.getElementById('confirmationModal');
+      confirmationModal.style.display = 'block';
+
+      // Handle confirm and cancel buttons for confirmation modal
+      const confirmButton = document.getElementById('confirmButton');
+      const cancelButton = document.getElementById('cancelButton');
+
+      confirmButton.onclick = () => {
+        // Code to book the slot and update the UI
+        bookSlot(slot, date, selectedTeam);
+        confirmationModal.style.display = 'none';
+      };
+
+      cancelButton.onclick = () => {
+        confirmationModal.style.display = 'none';
+      };
+    }
+  };
+
+  teamNameCancel.onclick = () => {
+    teamNameModal.style.display = 'none';
+  };
+};
+ 
+const bookSlot = (time, date, team) => {
+  // Find the content entry that matches the selected date
+  let selectedContent = content.find((item) => item.date === date);
+
+  // If no entry exists for the selected date, create a new one
+  if (!selectedContent) {
+    selectedContent = { date, slots: [] };
+    content.push(selectedContent);
+  }
+
+  // Add the booked slot to the selected date's slots
+  selectedContent.slots.push(time);
+
+  // Update local storage with the modified content
+  localStorage.setItem('bookedSlots', JSON.stringify(content));
+
+  // Mark the time slot as booked
+  const slotIndex = timeSlots.findIndex((slot) => slot.time === time);
+  if (slotIndex !== -1) {
+    timeSlots[slotIndex].booked = true;
+  }
+
+  // Re-render the calendar
+  renderCalendar();
+};
+
+  
+  
+// Click event listener for date cells
+monthDays.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('prev-date') || target.classList.contains('next-date')) {
+    // Clicked on a previous or next month date, handle as needed
+  } else if (target.classList.contains('today')) {
+    const currentDate = new Date(); // Get the current date
+    showDateContent(currentDate);   // Pass the current date to the function
+  } else {
+    const day = parseInt(target.textContent);
+    const selectedDate = new Date(date.getFullYear(), date.getMonth(), day);
+    showDateContent(selectedDate); // Pass the selected date to the function
+  }
+});
+
+// Click event listener for time slots
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('time-slot')) {
+    const slot = target.textContent;
+    const currentDate = new Date(); // Get the current date
+    showDateContent(currentDate);   // Pass the current date to the function
+    bookSlotConfirmation(slot, currentDate); // Call the confirmation function with the slot and current date
+  }
+});
+
+
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  yearLabel.querySelector("p").textContent = date.getFullYear();
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    const currentDate = new Date(date.getFullYear(), date.getMonth(), i);
+    const today = new Date();
+
+    if (
+      currentDate.toDateString() === today.toDateString() &&
+      date.getMonth() === today.getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+  }
+
+  const currentDate = new Date();
+
+  // Open the date container for the current date
+  showDateContent(currentDate);
+
+  monthDays.innerHTML = days;
+
+  // Click event listener for previous year
+  document.querySelector(".prev-year").addEventListener("click", () => {
+    date.setFullYear(date.getFullYear() - 1);
+    renderCalendar();
+  });
+
+  // Click event listener for next year
+  document.querySelector(".next-year").addEventListener("click", () => {
+    date.setFullYear(date.getFullYear() + 1);
+    renderCalendar();
+  });
+
+};
+
+
+
+// Click event listener for previous month
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  if (date.getMonth() === 11) {
+    date.setFullYear(date.getFullYear() - 1);
+  }
+  renderCalendar();
+});
+
+// Click event listener for next month
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  if (date.getMonth() === 0) {
+    date.setFullYear(date.getFullYear() + 1);
+  }
+  renderCalendar();
+});
+
+// Initial rendering of the calendar
+renderCalendar();
